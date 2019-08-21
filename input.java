e4f8537d4774e5e98a0f27635ab76be1a02ed0ab
Mandvirai <45147096+Mandvirai@users.noreply.github.com>
2019-08-21 14:28:58 +0530

    Add files via upload

---
+++ input.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class input {
+	public static void main(String[]args) {
+		double g = 9.8;
+		Scanner in = new Scanner(System.in);
+		double t = in.nextDouble();
+		double speed = g * t;
+		double dist = 0.5 *g* t * t;
+		System.out.println(speed);
+		System.out.println(dist);
+	}
+}
+  
